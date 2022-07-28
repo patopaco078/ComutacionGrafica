0c8bf95df50e42793db7a6185aa8dae5777226cb
patopaco078 <67531231+patopaco078@users.noreply.github.com>
2022-07-29 08:54:43 +0900

    flama

---
+++ Assets/Scripts/Flamethrower.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flamethrower : MonoBehaviour {
+
+    [SerializeField] private float attackDuration;
+    [SerializeField] private GameObject collider;
+    [SerializeField] private ParticleSystem flame;
+
+    private void Awake()
+    {
+        collider.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!collider.activeSelf)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                flame.Play();
+                collider.SetActive(true);
+                StartCoroutine("DisableCollider");
+            }
+        }
+    }
+
+    IEnumerator DisableCollider()
+    {
+        yield return new WaitForSeconds(attackDuration);
+        collider.SetActive(false);
+        flame.Stop();
+    }
+}
