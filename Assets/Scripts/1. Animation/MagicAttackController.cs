bb1bee3eabcdcc8c2125155f25b57ea40308984c
patopaco078 <67531231+patopaco078@users.noreply.github.com>
2022-07-29 09:26:44 +0900

    animation1

---
+++ Assets/Scripts/1. Animation/MagicAttackController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Animator))]
+public class MagicAttackController : MonoBehaviour {
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("IsAttack");
+        }
+    }
+}
